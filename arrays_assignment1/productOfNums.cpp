9648410a5ec29d493756dc8f450e6b568ed5f552
nydycodes <149265131+nydycodes@users.noreply.github.com>
2024-05-31 12:05:34 +0530

    Create productOfNums.cpp

---
+++ arrays_assignment1/productOfNums.cpp
@@ -0,0 +1,25 @@
+//product of numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter the no. of elements: ";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"enter the elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    int product = 1;
+    for(int i=0;i<n;i++){
+        product *= arr[i];
+    }
+    cout<<product<<endl;
+
+    return 0;
+}
+
